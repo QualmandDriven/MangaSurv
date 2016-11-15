d20bf9a61b3c147afd0561bbecac40b9e63537f9
QualmandDriven <a.schollwoeck@outlook.com>
2016-11-15 21:31:02 +0100

    CSS Overview

--- src/js/components/Manga.js
+++ src/js/components/Manga.js
@@ -25,39 +25,21 @@ export default class Manga extends React.Component {
     const { id, name, chapters, followed, lastupdate, image, chapterUpdates } = this.props;
     const imagePath = "images/" + image;
 
-    const imageStyle = {
-      height: "170px",
-      width: "100%",
-    };
-
-    const mangaStyle = {
-      width: "10em",
-      height: "16em",
-      border: 1,
-      display: "inline-block",
-    }
-
-    const nameStyle = {
-      width: "10em",
-      height: "3em",
-      display: "table-cell",
-      verticalAlign: "middle",
-      textAlign: "center",
-    }
-
     return (
-      <div style={mangaStyle}>
+      <div class="overview">
         {followed ? 
-          <button class="btn btn-sm btn-danger" onClick={this.unfollowManga.bind(this)}>- Unfollow</button>
+          <button class="btn btn-sm btn-danger hoverdeleteoverview" onClick={this.unfollowManga.bind(this)}>- Unfollow</button>
           :
-          <button class="btn btn-sm btn-primary" onClick={this.followManga.bind(this)}>+ Follow</button> 
+          <button class="btn btn-sm btn-primary hoveraddoverview" onClick={this.followManga.bind(this)}>+ Follow</button> 
         }
-        <img src={imagePath} style={imageStyle}/>
-        <div style={nameStyle}>
+        <a href="#">
+          <img src={imagePath}/>
+        </a>
+        <div>
           <table>
             <thead>
               <tr>
-                <th>{ name }</th>
+                <th colSpan="3">{ name }</th>
               </tr>
             </thead>
             <tbody>
