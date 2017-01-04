85694c79ca27faac897f1bc6005ce832d9cd24d6
QualmandDriven <QualmandDriven@outlook.com>
2017-01-04 22:51:11 +0100

    Formatted logging

--- mangasurvfetcher/Logging/Logging.cs
+++ mangasurvfetcher/Logging/Logging.cs
@@ -3,12 +3,15 @@ using NLog.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace mangasurvlib.Logging
 {
     public static class ApplicationLogging
     {
+        public const string _MARK = "===========================================";
+
         public static ILoggerFactory LoggerFactory { get; } = new LoggerFactory();
         public static ILogger CreateLogger<T>() =>
           LoggerFactory.CreateLogger<T>();
@@ -36,6 +39,21 @@ namespace mangasurvlib.Logging
             config.LoggingRules.Add(new NLog.Config.LoggingRule("*", NLog.LogLevel.Trace, consoleTarget));
 
             NLog.LogManager.Configuration = config;
+        }
+
+        public static void StartLogging(this ILogger logger, Assembly asb)
+        {
+            logger.LogInformation(_MARK);
+            logger.LogInformation("Date:\t\t'{0}'", DateTime.Now.ToString("dd.MM.yyyy"));
+            logger.LogInformation("User:\t\t'{0}'", System.Security.Claims.ClaimsPrincipal.Current);
+            logger.LogInformation("Application:\t'{0}'", asb.GetName().Name);
+            logger.LogInformation("Version:\t\t'{0}'", asb.GetName().Version.ToString());
+            logger.LogInformation("");
+        }
+
+        public static void EndLogging(this ILogger logger)
+        {
+            logger.LogInformation(_MARK);
         }
     }
 }
--- mangasurvfetcher/Program.cs
+++ mangasurvfetcher/Program.cs
@@ -1,12 +1,17 @@
+using Microsoft.Extensions.Logging;
+using mangasurvlib.Logging;
 using System;
 
 namespace mangasurvfetcher
 {
     public class Program
     {
+        private static ILogger logger = mangasurvlib.Logging.ApplicationLogging.CreateLogger<Program>();
+
         public static void Main(string[] args)
         {
             mangasurvlib.Logging.ApplicationLogging.ConfigureLogger();
+            logger.StartLogging(System.Reflection.Assembly.GetEntryAssembly());
 
             mangasurvlib.Manga.IMangaManager mangaManager = mangasurvlib.Manga.MangaFactory.CreateMangaManager();
             mangaManager.LoadMangas();
@@ -17,6 +22,8 @@ namespace mangasurvfetcher
             animeManager.LoadAnimes();
             //animeManager.LoadAnimeImages();
             animeManager.SearchNewEpisodes();
+            
+            logger.EndLogging();
         }
     }
 }
