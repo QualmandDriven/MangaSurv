b015ccbaa0163e1268a3e99e8c68f3198437ed5b
QualmandDriven <QualmandDriven@outlook.com>
2017-01-29 14:20:19 +0100

    Changed publish batch file

--- MangaSurvWebApi/src/MangaSurvWebApi/createDocker.sh
+++ MangaSurvWebApi/src/MangaSurvWebApi/createDocker.sh
@@ -3,3 +3,5 @@ dotnet publish -c Release -o out
 
 docker build -t mangasurv/webapi .
 docker save mangasurv/webapi > mangasurvwebapi_runtime.tar
+
+mv /home/paradise/Documents/mangasurv/MangaSurvWebApi/src/MangaSurvWebApi/mangasurvwebapi_runtime.tar /home/paradise/docker_homeautomationapi/mangasurvwebapi_runtime.tar
